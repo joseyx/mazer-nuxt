940d7e46ea270e39ce1ef03e3367c5c0d9072449
Jose Rojas <joseyx041199@gmail.com>
2023-08-23 01:26:39 -0400

    removed imports

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,12 @@
 <script lang="ts" setup>
-import LayoutHeader from '~/components/Layouts/LayoutHeader.vue';
-import LayoutFooter from '~/components/Layouts/LayoutFooter.vue';
-import LayoutSidebar from '~/components/Layouts/Sidebar/LayoutSidebar.vue';
+// import LayoutHeader from '~/components/Layouts/LayoutHeader.vue';
+// import LayoutFooter from '~/components/Layouts/LayoutFooter.vue';
+// import LayoutSidebar from '~/components/Layouts/Sidebar/LayoutSidebar.vue';
 </script>
 <template>
   <div id="app">
-    <LayoutSidebar></LayoutSidebar>
-      <div id="main">
-          <LayoutHeader></LayoutHeader>
-          <slot/>
-          <LayoutFooter></LayoutFooter>
-      </div>
+    <div id="main">
+      <slot />
+    </div>
   </div>
 </template>
